9c7219ef0a4b02cb85f3fc81ba2b9843753d0f20
Aditya Dawadikar <adityadawadikar2000@gmail.com>
2020-09-08 10:51:47 +0530

    trial one

--- js/registrationform.js
+++ js/registrationform.js
@@ -1,105 +1,93 @@
+function register() {
+    //personal info
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var name = document.getElementById("name").value;
+    var college = document.getElementById("college").value;
+    var department = document.getElementById("department").value;
+    var year = document.getElementById("year").value;
+    var division = document.getElementById("division").value;
+    var rollno = document.getElementById("rollno").value;
+    //social info
+    var phone = document.getElementById("phone").value;
+    var iswhatsapp = document.getElementById("isWhatsapp").value;
+    var linkedin = document.getElementById("linkedin").value;
+    var github = document.getElementById("github").value;
+    var personalwebsite = document.getElementById("personalWebsite").value;
+    var resume = document.getElementById("resume").value;
+    //skills
+    var primaryskill = document.getElementById("primarySkill").value;
+    var secondaryskill = document.getElementById("secondarySkill").value;
+    var skill = document.getElementById("otherskills").value;
+    var projectsforskills = document.getElementById("project").value;
+    var cgpa = document.getElementById("sgpa").value;
+    let skillArray = [];
+    skillArray.push(skill);
+    let projectArray = [];
+    projectArray.push(projectsforskills);
+    //optional info
+    var introduction = document.getElementById("introduction").value;
+    var gender = document.getElementById("gender").value;
+    var age = document.getElementById("age").value;
+    var mother_tongue = document.getElementById("motherTongue").value;
+    var languages_known = document.getElementById("knownLanguage").value;
+    let languageArray = [];
+    languageArray.push(languages_known);
 
-var counterskill = 1;
-function addskill(divName){
-    var newdiv = document.createElement('div');
-    var newlink = document.createElement('div');
-    newdiv.innerHTML = "Skill " + (counterskill + 1) + " <input type='text' name='skills[]'>";
-    newlink.innerHTML = " Link To the Project " + " <input type='text' name='skilllinks[]'><br><br>";
-    document.getElementById(divName).appendChild(newdiv);
-    document.getElementById(divName).appendChild(newlink);
-    counterskill++;    
-}
-
-var counterlanguage = 1;
-function addlanguage(divName){
-    var newdiv = document.createElement('div');
-    newdiv.innerHTML = "Language " + (counterlanguage + 1) + " : <input type='text' name='languages[]'>";
-    document.getElementById(divName).appendChild(newdiv);
-    counterlanguage++;    
-}
-
-function adduser(use1r){   
-
-    skillarr = [];
-    projectsforskills = []
-    var skill = document.getElementsByName('skills[]'); 
-    var link = document.getElementsByName('skilllinks[]'); 
-    for (var i = 0; i < skill.length; i++) { 
-        skillarr.push(skill[i].value)    
-        projectsforskills.push(link[i].value)    
+    personal = {
+        name: name,
+        college: college,
+        department: department,
+        year: year,
+        division: division,
+        rollno: rollno
     }
 
-    languagearr = []
-    var languages = document.getElementsByName('languages[]'); 
-    for (var i = 0; i < languages.length; i++) { 
-        languagearr.push(languages[i].value)      
+    social = {
+        phone: phone,
+        iswhatsapp: iswhatsapp,
+        linkedin: linkedin,
+        github: github,
+        personalwebsite: personalwebsite,
+        resume: resume
     }
-    
-    
-    var user = {
-        email : use1r.email.value,
-        password : use1r.pass.value,
-   
-        personal : {
-            name : use1r.name.value,
-            college : use1r.college.value,
-            deptartment : use1r.college.value,
-            year : use1r.year.value,
-            division : use1r.division.value,
-            rollno : use1r.rollno.value,
-        },
-        social: {
-            phone: use1r.phone.value,
-            linkedin: use1r.linkden.value,
-            github:use1r.github.value,
-            personalwebsite: use1r.personalsite.value,
-            resume: use1r.resume.value,
-            iswhatsaap: use1r.iswhatsapp.value
-        }, 
-        skills:{
-            skills : skillarr,
-            projectsforskills : projectsforskills,
-            topskill: use1r.topskill.value,
-            primaryskill : use1r.primaryskill.value,
-            secondaryskill : use1r.secondaryskill.value,
-            cgpa: use1r.cgpa.value
-        },
-        rating:use1r.rating.value,
-  
-        optionals: {
-            introduction: use1r.introduction.value,
-            gender: use1r.gender.value,
-            age : use1r.age.value,
-            mother_tongue: use1r.mothertongue.value,
-            languages_known : languagearr
-        },
-    };
-    
 
-    githublink = `https://api.github.com/users/${use1r.github.value.substr(use1r.github.value.lastIndexOf('/') + 1)}`
- 
-    fetch(githublink)
-        .then(response=>response.json())
-        .then(response=>{
-            user["metaData"] = response;
-        })
-        .then((response) => {
-            fetch("https://skboard.herokuapp.com/api/register/student",
-            {
-                method:'POST',
-                body:JSON.stringify(user),
-                headers: {
-                        'Content-Type':'application/json'
-                },
-                credentials:'same-origin'
-            })
-            .then(alert(JSON.stringify(user)))
-            .then(console.log(user))
-            .catch(alert("Falied To post"))
-        })
-        .catch(console.log("Failed to get git data"))
-    
-    alert(JSON.stringify(user));
-       
-    
+    skills = {
+        primaryskill: primaryskill,
+        secondaryskill: secondaryskill,
+        skill: skillArray,
+        projectsforskills: projectArray,
+        cgpa: cgpa
+    }
+
+    optionals = {
+        introduction: introduction,
+        gender: gender,
+        age: age,
+        mother_tongue: mother_tongue,
+        languages_known: languageArray,
+    }
+
+    //create user object
+    user = {
+            email: email,
+            password: password,
+            personal: personal,
+            social: social,
+            skills: skills,
+            optionals: optionals,
+            metaData: {}
+        }
+        //console.log(user)
+
+    let url = "https://skboard.herokuapp.com/api/register/student"
+
+    fetch(url, {
+        method: 'post',
+        body: JSON.stringify(user)
+    }).then(function(response) {
+        console.log(response.json());
+    }).catch(err => {
+        alert(err);
+    })
 }
