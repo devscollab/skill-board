4aeae17580f9ebed3db2365d141c5c5d873c49d8
Yash0411 <ynm0411@gmail.com>
2020-09-09 16:02:28 +0530

    Updated Reg Form

--- js/registrationform.js
+++ js/registrationform.js
@@ -1,4 +1,6 @@
 
+// Function to add data fields to skill array and its links 
+
 var counterskill = 1;
 function addskill(divName){
     var newdiv = document.createElement('div');
@@ -10,6 +12,9 @@ function addskill(divName){
     counterskill++;    
 }
 
+
+// Function to add data fields to language array
+
 var counterlanguage = 1;
 function addlanguage(divName){
     var newdiv = document.createElement('div');
@@ -18,8 +23,43 @@ function addlanguage(divName){
     counterlanguage++;    
 }
 
-function adduser(use1r){   
 
+
+
+
+// Function to post data
+
+async function postData(url , data) {
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      //credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+      },
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      //body: body // body data type must match "Content-Type" header
+      body: JSON.stringify(data),
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+
+
+
+
+  // Declare form object
+
+const registrationform = document.getElementById("registrationform")
+
+
+// Handle form events such as onsubmit
+// Event listner
+registrationform.addEventListener('submit',  function(e) {
+    e.preventDefault();
+
+    // Get the skills and links to projects array
     skillarr = [];
     projectsforskills = []
     var skill = document.getElementsByName('skills[]'); 
@@ -29,77 +69,71 @@ function adduser(use1r){
         projectsforskills.push(link[i].value)    
     }
 
+    // Get the languages array
     languagearr = []
     var languages = document.getElementsByName('languages[]'); 
     for (var i = 0; i < languages.length; i++) { 
         languagearr.push(languages[i].value)      
     }
     
-    
-    var user = {
-        email : use1r.email.value,
-        password : use1r.pass.value,
-   
-        personal : {
-            name : use1r.name.value,
-            college : use1r.college.value,
-            deptartment : use1r.college.value,
-            year : use1r.year.value,
-            division : use1r.division.value,
-            rollno : use1r.rollno.value,
-        },
-        social: {
-            phone: use1r.phone.value,
-            linkedin: use1r.linkden.value,
-            github:use1r.github.value,
-            personalwebsite: use1r.personalsite.value,
-            resume: use1r.resume.value,
-            iswhatsaap: use1r.iswhatsapp.value
-        }, 
-        skills:{
-            skills : skillarr,
-            projectsforskills : projectsforskills,
-            topskill: use1r.topskill.value,
-            primaryskill : use1r.primaryskill.value,
-            secondaryskill : use1r.secondaryskill.value,
-            cgpa: use1r.cgpa.value
-        },
-        rating:use1r.rating.value,
-  
-        optionals: {
-            introduction: use1r.introduction.value,
-            gender: use1r.gender.value,
-            age : use1r.age.value,
-            mother_tongue: use1r.mothertongue.value,
-            languages_known : languagearr
-        },
-    };
-    
 
-    githublink = `https://api.github.com/users/${use1r.github.value.substr(use1r.github.value.lastIndexOf('/') + 1)}`
+    const use1r = new FormData(this);
+    // Object To be posted
+    var user = {
+        "email" : this.email.value,
+        "password" : this.pass.value,
+        "personal" : {
+            "name" : this.name.value,
+            "college" : this.college.value,
+            "deptartment" : this.college.value,
+            "year" : this.year.value,
+            "division" : this.division.value,
+            "rollno" : this.rollno.value,
+        },
+        "social": {
+            "phone": this.phone.value,
+            "linkedin": this.linkden.value,
+            "github":this.github.value,
+            "personalwebsite": this.personalsite.value,
+            "resume": this.resume.value,
+            "iswhatsaap": this.iswhatsapp.value
+        }, 
+        "skills":{
+            "skills" : skillarr,
+            "projectsforskills" : projectsforskills,
+            "topskill": this.topskill.value,
+            "primaryskill" : this.primaryskill.value,
+            "secondaryskill" : this.secondaryskill.value,
+            "cgpa": this.cgpa.value
+        },
+        "rating":this.rating.value,
+        "optionals": {
+            "introduction": this.introduction.value,
+            "gender": this.gender.value,
+            "age" : this.age.value,
+            "mother_tongue": this.mothertongue.value,
+            "languages_known" : languagearr
+        },
+        "metaData":{
+        }
+    };
+
+
+    githublink = `https://api.github.com/users/${this.github.value.substr(this.github.value.lastIndexOf('/') + 1)}`
  
     fetch(githublink)
         .then(response=>response.json())
         .then(response=>{
-            user["metaData"] = response;
+            user["metaData"]["github_metadata_object"] = response;
         })
-        .then((response) => {
-            fetch("https://skboard.herokuapp.com/api/register/student",
-            {
-                method:'POST',
-                body:JSON.stringify(user),
-                headers: {
-                        'Content-Type':'application/json'
-                },
-                credentials:'same-origin'
-            })
-            .then(alert(JSON.stringify(user)))
-            .then(console.log(user))
-            .catch(alert("Falied To post"))
+        .then(async (response) => {
+
+            let url = "https://skboard.herokuapp.com/api/register/student";
+
+            const res = await postData(url, user);
+            console.log("Response =>" + JSON.stringify(res));
+           
         })
         .catch(console.log("Failed to get git data"))
-    
-    alert(JSON.stringify(user));
-       
-    
-}
+
+})
