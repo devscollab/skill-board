9a3981a08db98e1268293823255445ee073925e9
Aditya Mahajan <66865722+adityamahajan-io@users.noreply.github.com>
2020-09-09 19:16:53 +0530

    Update registrationform.js

--- js/registrationform.js
+++ js/registrationform.js
@@ -1,144 +1,123 @@
-
-// Function to add data fields to skill array and its links 
-
-var counterskill = 1;
-function addskill(divName){
-    var newdiv = document.createElement('div');
-    var newlink = document.createElement('div');
-    newdiv.innerHTML = "Skill " + (counterskill + 1) + " <input type='text' name='skills[]'>";
-    newlink.innerHTML = " Link To the Project " + " <input type='text' name='skilllinks[]'><br><br>";
-    document.getElementById(divName).appendChild(newdiv);
-    document.getElementById(divName).appendChild(newlink);
-    counterskill++;    
-}
-
-
-// Function to add data fields to language array
-
-var counterlanguage = 1;
-function addlanguage(divName){
-    var newdiv = document.createElement('div');
-    newdiv.innerHTML = "Language " + (counterlanguage + 1) + " : <input type='text' name='languages[]'>";
-    document.getElementById(divName).appendChild(newdiv);
-    counterlanguage++;    
-}
-
-
-
-
-
-// Function to post data
-
-async function postData(url , data) {
-    const response = await fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      //credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-      },
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      //body: body // body data type must match "Content-Type" header
-      body: JSON.stringify(data),
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
+async function register() {
+  //personal info
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var name = document.getElementById("name").value;
+  var college = document.getElementById("college").value;
+  var department = document.getElementById("department").value;
+  var year = document.getElementById("year").value;
+  var division = document.getElementById("division").value;
+  var rollno = document.getElementById("rollno").value;
+  //social info
+  var phone = document.getElementById("phone").value;
+  var flag = document.getElementById("isWhatsapp").value;
+  var iswhatsapp = false;
+  if (flag === "on") {
+    iswhatsapp = true;
   }
+  var linkedin = document.getElementById("linkedin").value;
+  var github = document.getElementById("github").value;
+  var personalwebsite = document.getElementById("personalWebsite").value;
+  var resume = document.getElementById("resume").value;
+  //skills
+  var primaryskill = document.getElementById("primarySkill").value;
+  var secondaryskill = document.getElementById("secondarySkill").value;
+  var skill = document.getElementById("otherskills").value;
+  var projectsforskills = document.getElementById("project").value;
+  var cgpa = document.getElementById("sgpa").value;
+  let skillArray = [];
+  skillArray.push(skill);
+  let projectArray = [];
+  projectArray.push(projectsforskills);
+  //optional info
+  var introduction = document.getElementById("introduction").value;
+  var gender = document.getElementById("gender").value;
+  var age = document.getElementById("age").value;
+  var mother_tongue = document.getElementById("motherTongue").value;
+  var languages_known = document.getElementById("knownLanguage").value;
+  let languageArray = [];
+  languageArray.push(languages_known);
 
-
-
-
-
-  // Declare form object
-
-const registrationform = document.getElementById("registrationform")
-
-
-// Handle form events such as onsubmit
-// Event listner
-registrationform.addEventListener('submit',  function(e) {
-    e.preventDefault();
-
-    // Get the skills and links to projects array
-    skillarr = [];
-    projectsforskills = []
-    var skill = document.getElementsByName('skills[]'); 
-    var link = document.getElementsByName('skilllinks[]'); 
-    for (var i = 0; i < skill.length; i++) { 
-        skillarr.push(skill[i].value)    
-        projectsforskills.push(link[i].value)    
-    }
-
-    // Get the languages array
-    languagearr = []
-    var languages = document.getElementsByName('languages[]'); 
-    for (var i = 0; i < languages.length; i++) { 
-        languagearr.push(languages[i].value)      
-    }
-    
-    var iswhatsapp = false;
-    if (this.iswhatsapp.value === "on") {
-        iswhatsapp = true;
-    }
-    
-
-    const use1r = new FormData(this);
-    // Object To be posted
-    var user = {
-        "email" : this.email.value,
-        "password" : this.pass.value,
-        "personal" : {
-            "name" : this.name.value,
-            "college" : this.college.value,
-            "department" : this.college.value,
-            "year" : this.year.value,
-            "division" : this.division.value,
-            "rollno" : this.rollno.value,
-        },
-        "social": {
-            "phone": this.phone.value,
-            "linkedin": this.linkden.value,
-            "github":this.github.value,
-            "personalwebsite": this.personalsite.value,
-            "resume": this.resume.value,
-            "iswhatsapp": iswhatsapp
-        }, 
-        "skills":{
-            "skills" : skillarr,
-            "projectsforskills" : projectsforskills,
-            "topskill": this.topskill.value,
-            "primaryskill" : this.primaryskill.value,
-            "secondaryskill" : this.secondaryskill.value,
-            "cgpa": this.cgpa.value
-        },
-        "rating":this.rating.value,
-        "optionals": {
-            "introduction": this.introduction.value,
-            "gender": this.gender.value,
-            "age" : this.age.value,
-            "mother_tongue": this.mothertongue.value,
-            "languages_known" : languagearr
-        },
-        "metaData":{
-        }
+    personal = {
+      name: name,
+      college: college,
+      department: department,
+      year: year,
+      division: division,
+      rollno: rollno,
     };
 
+    social = {
+      phone: phone,
+      iswhatsapp: iswhatsapp,
+      linkedin: linkedin,
+      github: github,
+      personalwebsite: personalwebsite,
+      resume: resume,
+    };
 
-    githublink = `https://api.github.com/users/${this.github.value.substr(this.github.value.lastIndexOf('/') + 1)}`
- 
-    fetch(githublink)
-        .then(response=>response.json())
-        .then(response=>{
-            user["metaData"]["github_metadata_object"] = response;
-        })
-        .then(async (response) => {
+    // Just complete the logic of array
 
-            let url = "https://skboard.herokuapp.com/api/register/student";
+  skills = {
+    primaryskill: primaryskill,
+    secondaryskill: secondaryskill,
+    skill: ["nodejs", "java"],
+    projectsforskills: ["https://github.com/Aditya-Dawadikar", "https://github.com/Aditya-Dawadikar"],
+    cgpa: cgpa,
+  };
 
-            const res = await postData(url, user);
-            console.log("Response =>" + JSON.stringify(res));
-           
-        })
-        .catch(console.log(""))
+  optionals = {
+    introduction: introduction,
+    gender: gender,
+    age: age,
+    mother_tongue: mother_tongue,
+    languages_known: ['marathi', 'english'],
+  };
 
-})
+  // fetching github metaData 
+  const github_metadata_object =  await getGithubData();
+  
+  //create user object
+  user = {
+    email: email,
+    password: password,
+    personal: personal,
+    social : social,
+    skills: skills,
+    optionals : optionals,
+    metaData: {
+      github_metadata_object:github_metadata_object
+    },
+  };
+
+  // url to send post req
+  let url = "https://skboard.herokuapp.com/api/register/student";
+
+  const response = await postData(url,user);
+  console.log("Response =>" + response);
+}
+
+// Function to fetch github data
+async function getGithubData(user){
+  const url =  `https://api.github.com/users/${social.github.substr(social.github.lastIndexOf('/') + 1)}`
+  const data =await fetch(url);
+  const json = await data.json();
+  return json
+}
+
+// Function to post data
+async function postData(url = "", data) {
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    //credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    //body: body // body data type must match "Content-Type" header
+    body: JSON.stringify(data),
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
