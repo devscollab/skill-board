daa9f17bd02f465c118c3e73ef5493c05100c62c
Aditya Mahajan <adityamahajan.tech@gmail.com>
2020-09-10 19:28:37 +0530

    minor fixes

--- js/profileActions.js
+++ js/profileActions.js
@@ -26,6 +26,22 @@ function deleteProfile(user){
     }) 
     .then(response => response.json()) 
     .then(json => console.log(json));
-    alert("deleted" +user);
+    alert("deleted " +user);
+    location.reload()
+}
+
+function deleteUnverified(user){
+    const url = "https://skboard.herokuapp.com/api/unverified/delete/" +user
+    console.log(url) 
+    fetch(url, { 
+        method: "DELETE", 
+        body: null,
+        headers: { 
+            "Content-type": "application/json;"
+        } 
+    }) 
+    .then(response => response.json()) 
+    .then(json => console.log(json));
+    alert("deleted " +user);
     location.reload()
 }
