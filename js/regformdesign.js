a0fb8359794f3eded15c40f6fcbe94ac78ac5902
Yash0411 <ynm0411@gmail.com>
2020-09-15 22:44:50 +0530

    Design Finalized

---
+++ js/regformdesign.js
@@ -0,0 +1,39 @@
+const  previousBtn  =  document.getElementById('previousBtn');
+const  nextBtn  =  document.getElementById('nextBtn');
+const  content  =  document.getElementById('content');
+const  bullets  =  [...document.querySelectorAll('.bullet')];
+const formfields = [...document.querySelectorAll('.part')];
+
+const MAX_STEPS = 4;
+let currentStep = 1;
+
+nextBtn.addEventListener('click',  ()  =>  {
+    bullets[currentStep  -  1].classList.add('completed');
+	currentStep  +=  1;
+    previousBtn.disabled  =  false;
+    if (currentStep  !==  1) {
+        formfields[currentStep  -  2].classList.add('hide');
+    }
+	if  (currentStep  ===  MAX_STEPS)  {
+        nextBtn.disabled  =  true;
+	}
+    formfields[currentStep -1].classList.remove('hide');
+});
+
+
+previousBtn.addEventListener('click',  ()  =>  {
+	bullets[currentStep  -  2].classList.remove('completed');
+    formfields[currentStep  -  1].classList.add('hide');
+	currentStep  -=  1;
+	nextBtn.disabled  =  false;
+    if (currentStep  !==  MAX_STEPS) {
+        formfields[currentStep  - 1].classList.remove('hide');
+    }
+	if  (currentStep  ===  1)  {
+        previousBtn.disabled  =  true;
+	}
+});
+
+previousBtn.disabled  =  true;
+
+
